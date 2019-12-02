6cb02812e2a683defd89ad2a9a30852a26bad393
AnjalaD <anjaladilhara@gmail.com>
2019-12-02 14:23:43 +0530

    struct fix

--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { guestRoutes } from './routes';
+import { guestRoutes, adminRoutes, hrRoutes, employeeRoutes } from './routes';
+import { ADMIN, HR } from './constants/constants';
 
 
 function App() {
-  const loggedIn = useSelector(state => state.user.loggedIn);
+  const { loggedIn, user } = useSelector(state => state.user);
 
   const createRoute = (routes) => routes.map(
     (index, route) => (
-      <Route path={route.path} component={route.component} />
+      <Route path={route.path} component={route.component} key={index} />
     )
   );
+
+
+  const userRouteSelector = (type) => {
+    switch (type) {
+      case ADMIN:
+        return createRoute(adminRoutes);
+      case HR:
+        return createRoute(hrRoutes);
+      default:
+        return createRoute(employeeRoutes);
+    }
+  }
 
 
   return (
     <BrowserRouter>
       <Switch>
-        {loggedIn ? null : createRoute(guestRoutes)}
+        {loggedIn ? userRouteSelector(user.type) : createRoute(guestRoutes)}
       </Switch>
     </BrowserRouter>
   );
