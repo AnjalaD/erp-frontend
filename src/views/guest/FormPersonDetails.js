fe4a3b1099d55f41bb65fabec43bc32b2787e2b0
chamikaCN <chamikanandasiri97@gmail.com>
2019-12-04 18:53:37 +0530

    forms modify

--- src/views/guest/FormPersonDetails.js
+++ src/views/guest/FormPersonDetails.js
@@ -19,58 +19,50 @@ export class FormPersonalDetails extends Component {
     render() {
         const { values, handleChange } = this.props;
         return (
-            <MuiThemeProvider >
-                <React.Fragment>
-                    <Dialog
-                        open="true"
+            <div>
+                    <AppBar title="Enter Personal Details" />
+                    <TextField
+                        placeholder="Enter Your Occupation"
+                        label="Occupation"
+                        onChange={handleChange('occupation')}
+                        defaultValue={values.occupation}
+                        margin="normal"
                         fullWidth="true"
-                        maxWidth='sm'
-                    >
-                        <AppBar title="Enter Personal Details" />
-                        <TextField
-                            placeholder="Enter Your Occupation"
-                            label="Occupation"
-                            onChange={handleChange('occupation')}
-                            defaultValue={values.occupation}
-                            margin="normal"
-                            fullWidth="true"
-                        />
-                        <br />
-                        <TextField
-                            placeholder="Enter Your City"
-                            label="City"
-                            onChange={handleChange('city')}
-                            defaultValue={values.city}
-                            margin="normal"
-                            fullWidth="true"
-                        />
-                        <br />
-                        <TextField
-                            placeholder="Enter Your Bio"
-                            label="Bio"
-                            onChange={handleChange('bio')}
-                            defaultValue={values.bio}
-                            margin="normal"
-                            fullWidth="true"
-                        />
-                        <br />
+                    />
+                    <br />
+                    <TextField
+                        placeholder="Enter Your City"
+                        label="City"
+                        onChange={handleChange('city')}
+                        defaultValue={values.city}
+                        margin="normal"
+                        fullWidth="true"
+                    />
+                    <br />
+                    <TextField
+                        placeholder="Enter Your Bio"
+                        label="Bio"
+                        onChange={handleChange('bio')}
+                        defaultValue={values.bio}
+                        margin="normal"
+                        fullWidth="true"
+                    />
+                    <br />
 
-                        <Button
-                            color="secondary"
-                            variant="contained"
-                            onClick={this.back}
-                        >Back</Button>
+                    <Button
+                        color="secondary"
+                        variant="contained"
+                        onClick={this.back}
+                    >Back</Button>
 
-                        <Button
-                            color="primary"
-                            variant="contained"
-                            onClick={this.continue}
-                        >Continue</Button>
-                    </Dialog>
-                </React.Fragment>
-            </MuiThemeProvider>
-        );
-    }
-}
-
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        onClick={this.continue}
+                    >Continue</Button>
+                        </div>
+                );
+            }
+        }
+        
 export default FormPersonalDetails;
