6f17453740a2edfa25b3fa47720e6ef4e20d4b86
AnjalaD <anjaladilhara@gmail.com>
2019-12-06 16:40:34 +0530

    confirm form view

--- src/views/guest/Confirm.js
+++ src/views/guest/Confirm.js
@@ -1,64 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
-import AppBar from '@material-ui/core/AppBar';
-import { List, ListItem, ListItemText } from '@material-ui/core/';
+import { Card } from '@material-ui/core/';
 import Button from '@material-ui/core/Button';
+import Profile from '../../components/profile/Profile';
 
-export class Confirm extends Component {
-    continue = e => {
-        e.preventDefault();
-        // PROCESS FORM //
-        this.props.nextStep();
-    };
+function Confirm(props) {
+    const { user, emg, dep } = props;
+    console.info(user);
 
-    back = e => {
-        e.preventDefault();
-        this.props.prevStep();
-    };
+    return (
+        <Card>
+            <Profile data={user} />
 
-    render() {
-        const {
-            values: { firstName, lastName, email, occupation, city, bio }
-        } = this.props;
-        return (
-            <div>
-                <AppBar title="Confirm User Data" />
-                <List>
-                    <ListItem>
-                        <ListItemText primary="First Name" secondary={firstName} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="Last Name" secondary={lastName} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="Email" secondary={email} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="Occupation" secondary={occupation} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="City" secondary={city} />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="Bio" secondary={bio} />
-                    </ListItem>
-                </List>
-                <br />
+            <Button
+                color="secondary"
+                variant="contained"
+            >Back</Button>
 
-                <Button
-                    color="secondary"
-                    variant="contained"
-                    onClick={this.back}
-                >Back</Button>
-
-                <Button
-                    color="primary"
-                    variant="contained"
-                    onClick={this.continue}
-                >Confirm & Continue</Button>
-            </div>
-        );
-    }
+            <Button
+                color="primary"
+                variant="contained"
+            >Confirm & Continue</Button>
+        </Card>
+    );
 }
 
 export default Confirm;
