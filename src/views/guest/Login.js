e34efc877a3adc434c4b0818c577defe1c4febbc
AnjalaD <anjaladilhara@gmail.com>
2019-12-06 15:42:31 +0530

    dependancy fix

--- src/views/guest/Login.js
+++ src/views/guest/Login.js
@@ -11,7 +11,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { LOGIN } from '../../constants/constants';
+import { LOGIN } from '../../constants/api';
 import { useDispatch } from 'react-redux';
 import { login, loading } from '../../redux/actions';
 import Cookies from 'js-cookie';
