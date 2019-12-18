5c2404b928014f248e7813201ec8cdfe7516bb2c
AnjalaD <anjaladilhara@gmail.com>
2019-12-18 15:21:37 +0530

    remove redundent parts

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,10 @@
 
 import { combineReducers } from 'redux';
-import userReducer from './userReducer';
 import statusReducer from './statusReducer';
 import loadingReducer from './loadingReducer';
 
 const allReducers = combineReducers({
     status: statusReducer,
-    user: userReducer,
     loading: loadingReducer
 });
 
