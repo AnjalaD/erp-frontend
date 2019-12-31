1c701d70dc49236b8b5262331b920510fd6d34be
Anjala <anjaladilhara@gmail.com>
2019-12-31 09:19:17 +0530

    cleanup profile view

--- src/views/level_1/UserProfile.js
+++ src/views/level_1/UserProfile.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loading } from '../../redux/actions';
-import Profile from '../../components/profile/Profile';
 import { GET_USER } from '../../constants/api';
-import { Grid, Container, Typography } from '@material-ui/core';
-import EmgContact from '../../components/profile/EmgContact';
+import { Container } from '@material-ui/core';
 import { fetchData, makeOptions } from '../../util/helper';
+import FullProfile from '../../components/profile/FullProfile';
 
 function UserProfile() {
     const dispatch = useDispatch();
@@ -26,43 +25,7 @@ function UserProfile() {
         <Container maxWidth='md'>
             {
                 profile ?
-                    <Grid container direction='column' alignItems='center'>
-                        <Grid item xs={12}>
-                            <Typography variant='h5'>
-                                Basic Info
-                            </Typography>
-                        </Grid>
-                        < Grid item xs={12} >
-                            <Profile data={{
-                                ...profile.employee,
-                                custom: profile.custom
-                            }} />
-                        </ Grid>
-                        <Grid item xs={12}>
-                            <Typography variant='h6' style={{ marginTop: 32 }}>
-                                Dependents Info
-                            </Typography>
-                        </Grid>
-                        {
-                            profile.dependents.map((dep, i) => (
-                                <Grid item xs={12} key={i}>
-                                    <Profile data={dep} />
-                                </Grid>
-                            ))
-                        }
-                        <Grid item xs={12} style={{ marginTop: 32 }}>
-                            <Typography variant='h6'>
-                                Emergency Contacts
-                            </Typography>
-                        </Grid>
-                        {
-                            profile.emergency_contacts.map((emg, i) => (
-                                <Grid item xs={12} key={i}>
-                                    <EmgContact data={emg} />
-                                </Grid>
-                            ))
-                        }
-                    </Grid >
+                    <FullProfile profile={profile} />
                     :
                     'Error retriving data'
             }
