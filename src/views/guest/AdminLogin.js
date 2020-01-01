1720dd3d330917c352bea0ff8775b1022ba6a57b
Anjala <anjaladilhara@gmail.com>
2020-01-01 17:33:34 +0530

    api end point fix

--- src/views/guest/AdminLogin.js
+++ src/views/guest/AdminLogin.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Login from './Login';
+import { ADMIN_LOGIN } from '../../constants/api';
 
 function AdminLogin() {
-    return <Login loginApi={AdminLogin} label='Admin Sign In' />
+    return <Login loginApi={ADMIN_LOGIN} label='Admin Sign In' />
 }
 
 export default AdminLogin;
